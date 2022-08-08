a0f28d384833e205d48fcfe0690d8e41c7652ddf
Kim <john0714@naver.com>
2022-08-09 00:09:23 +0900

    update test

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/hello', function() {
+Route::get('/hello', function () {
     return response('<h1>Hello!World</h1>', 200)
         ->header('Content-Type', 'text/plain')
         ->header('foo', 'bar');
 });
 
-Route::get('/posts/{id}', function($id){
-    return response('Post ' . $id);
+Route::get('/posts/{id}', function ($id) {
+    return response('Post' . $id);
 })->where('id', '[0-9]+');
